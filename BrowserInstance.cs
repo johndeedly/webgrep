b5f7956f309767caa38d79986bc1c5461d10659d
webgrep <webgrep>
2021-02-26 11:12:11 +0100

    Added two methods to get the html-aware text of elements

--- BrowserInstance.cs
+++ BrowserInstance.cs
@@ -9,6 +9,8 @@ namespace webgrep
     {
         void ClickOn(string selector, int? timeout = 5000);
         bool ElementIsAttached(string selector, int? timeout = 5000);
+        string GetFormattedText(string selector, int? timeout = 5000);
+        List<string> GetFormattedTextAll(string selector, int? timeout = 5000);
         string GetText(string selector, int? timeout = 5000);
         List<string> GetTextAll(string selector, int? timeout = 5000);
         void KeyboardPressKey(string key);
@@ -86,6 +88,38 @@ namespace webgrep
                 foreach (var elem in elements)
                 {
                     string text = elem.GetTextContentAsync(timeout).GetAwaiter().GetResult();
+                    list.Add(text.Trim());
+                }
+            }
+            catch (TimeoutException ex)
+            {
+                list.Add(ex.Message);
+            }
+            return list;
+        }
+
+        public string GetFormattedText(string selector, int? timeout = 5000)
+        {
+            try
+            {
+                string text = page.GetInnerTextAsync(selector, timeout).GetAwaiter().GetResult();
+                return text.Trim();
+            }
+            catch (TimeoutException ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public List<string> GetFormattedTextAll(string selector, int? timeout = 5000)
+        {
+            List<string> list = new List<string>();
+            try
+            {
+                List<IElementHandle> elements = page.QuerySelectorAllAsync(selector).GetAwaiter().GetResult().ToList();
+                foreach (var elem in elements)
+                {
+                    string text = elem.GetInnerTextAsync(timeout).GetAwaiter().GetResult();
                     list.Add(text.Trim());
                 }
             }
